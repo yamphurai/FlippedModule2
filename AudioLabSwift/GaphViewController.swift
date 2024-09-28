31e2904dbd8279491daeb9521fec527ac929a346
Jeevan Rai <yamphurai007@yahoo.com>
2024-09-27 21:47:17 -0500

    Added the graph of 20 random samples from the time domain audio data Added the stop and paus methods to stop/pause audio processing in AudioModel (may need to be revisited)

---
+++ AudioLabSwift/GaphViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GaphViewController.swift
+//  AudioLabSwift
+//
+//  Created by Reshma Shrestha on 9/27/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class GaphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
